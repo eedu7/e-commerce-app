74e44c9c463d3021888df6f21e1562e41eaa8ae8
eedu7 <mueedahmad067@gmail.com>
2025-09-22 15:23:50 +0500

    Modify some settings

--- apps/mobile/lib/presentations/screens/login_screen.dart
+++ apps/mobile/lib/presentations/screens/login_screen.dart
@@ -26,63 +26,87 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextFormField(
-                controller: emailController,
-                decoration: const InputDecoration(labelText: 'Email'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Email is required';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                controller: passwordController,
-                obscureText: !isPasswordVisible,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      isPasswordVisible
-                          ? Icons.visibility_off
-                          : Icons.visibility,
-                    ),
-                    onPressed: () => {
-                      setState(() {
-                        isPasswordVisible = !isPasswordVisible;
-                      }),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 24.0,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Text(
+                  'Welcome back,',
+                  style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
+                ),
+                Text('Good to see you again'),
+              ],
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextFormField(
+                    controller: emailController,
+                    decoration: const InputDecoration(labelText: 'Email'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Email is required';
+                      }
+                      return null;
                     },
                   ),
-                ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: !isPasswordVisible,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          isPasswordVisible
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
+                        onPressed: () => {
+                          setState(() {
+                            isPasswordVisible = !isPasswordVisible;
+                          }),
+                        },
+                      ),
+                    ),
 
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Password is required";
-                  }
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Password is required';
+                      }
 
-                  return null;
-                },
+                      return null;
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  TextButton(
+                    onPressed: () => {
+                      if (_formKey.currentState!.validate())
+                        {context.go('/home')},
+                    },
+                    child: const Text('Login'),
+                  ),
+                  const SizedBox(height: 20),
+                  TextButton(
+                    onPressed: () => context.go('/register'),
+                    child: const Text('Don\'t have an account? Register'),
+                  ),
+                ],
               ),
-              const SizedBox(height: 20),
-              TextButton(
-                onPressed: () => {
-                  if (_formKey.currentState!.validate()) {context.go('/home')},
-                },
-                child: const Text('Login'),
-              ),
-              const SizedBox(height: 20),
-              TextButton(
-                onPressed: () => context.go('/register'),
-                child: const Text('Don\'t have an account? Register'),
-              ),
-            ],
-          ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 32.0,
+              children: <Widget>[
+                IconButton(onPressed: () {}, icon: Icon(Icons.g_mobiledata)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.facebook)),
+              ],
+            ),
+          ],
         ),
       ),
     );
--- apps/mobile/lib/presentations/screens/register_screen.dart
+++ apps/mobile/lib/presentations/screens/register_screen.dart
@@ -26,85 +26,111 @@ class _RegisterScreenState extends State<RegisterScreen> {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextFormField(
-                controller: emailController,
-                decoration: const InputDecoration(labelText: 'Email'),
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Email is required';
-                  }
-                  return null;
-                },
-              ),
-              const SizedBox(height: 20),
-              TextFormField(
-                controller: passwordController,
-                obscureText: !isPasswordVisible,
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  suffixIcon: IconButton(
-                    icon: Icon(
-                      isPasswordVisible
-                          ? Icons.visibility_off
-                          : Icons.visibility,
-                    ),
-                    onPressed: () => {
-                      setState(() {
-                        isPasswordVisible = !isPasswordVisible;
-                      }),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 24.0,
+          children: [
+            Column(
+              children: <Widget>[
+                Text(
+                  'Hello there,',
+                  style: TextStyle(fontSize: 32.0, fontWeight: FontWeight.bold),
+                ),
+                Text('We are excited to see you here'),
+              ],
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  TextFormField(
+                    controller: emailController,
+                    decoration: const InputDecoration(labelText: 'Email'),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Email is required';
+                      }
+                      return null;
                     },
                   ),
-                ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    controller: passwordController,
+                    obscureText: !isPasswordVisible,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          isPasswordVisible
+                              ? Icons.visibility_off
+                              : Icons.visibility,
+                        ),
+                        onPressed: () => {
+                          setState(() {
+                            isPasswordVisible = !isPasswordVisible;
+                          }),
+                        },
+                      ),
+                    ),
 
-                validator: (value) {
-                  if (value == null || value.isEmpty) {
-                    return "Password is required";
-                  }
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Password is required";
+                      }
 
-                  final errors = <String>[];
-                  if (value.length < 8) {
-                    errors.add("At least 8 characters");
-                  }
-                  if (value.length > 16) {
-                    errors.add('At most 16 characters');
-                  }
-                  if (!RegExp(r'[A-Z]').hasMatch(value)) {
-                    errors.add('At least 1 uppercase letter');
-                  }
-                  if (!RegExp(r'[a-z]').hasMatch(value)) {
-                    errors.add('At least 1 lowercase letter');
-                  }
-                  if (!RegExp(r'\d').hasMatch(value)) {
-                    errors.add('At least 1 number');
-                  }
-                  if (!RegExp(r'[!@#\$&9~.,;:()_\-+=<>?]').hasMatch(value)) {
-                    errors.add('At least 1 special character');
-                  }
+                      final errors = <String>[];
+                      if (value.length < 8) {
+                        errors.add("At least 8 characters");
+                      }
+                      if (value.length > 16) {
+                        errors.add('At most 16 characters');
+                      }
+                      if (!RegExp(r'[A-Z]').hasMatch(value)) {
+                        errors.add('At least 1 uppercase letter');
+                      }
+                      if (!RegExp(r'[a-z]').hasMatch(value)) {
+                        errors.add('At least 1 lowercase letter');
+                      }
+                      if (!RegExp(r'\d').hasMatch(value)) {
+                        errors.add('At least 1 number');
+                      }
+                      if (!RegExp(
+                        r'[!@#\$&9~.,;:()_\-+=<>?]',
+                      ).hasMatch(value)) {
+                        errors.add('At least 1 special character');
+                      }
 
-                  if (errors.isEmpty) return null;
+                      if (errors.isEmpty) return null;
 
-                  return errors.join('\n');
-                },
+                      return errors.join('\n');
+                    },
+                  ),
+                  const SizedBox(height: 20),
+                  TextButton(
+                    onPressed: () => {
+                      if (_formKey.currentState!.validate())
+                        {context.go('/home')},
+                    },
+                    child: const Text('Register'),
+                  ),
+                  const SizedBox(height: 20),
+                  TextButton(
+                    onPressed: () => context.go('/login'),
+                    child: const Text('Already have an account? Login'),
+                  ),
+                ],
               ),
-              const SizedBox(height: 20),
-              TextButton(
-                onPressed: () => {
-                  if (_formKey.currentState!.validate()) {context.go('/home')},
-                },
-                child: const Text('Register'),
-              ),
-              const SizedBox(height: 20),
-              TextButton(
-                onPressed: () => context.go('/login'),
-                child: const Text('Already have an account? Login'),
-              ),
-            ],
-          ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              spacing: 32.0,
+              children: <Widget>[
+                IconButton(onPressed: () {}, icon: Icon(Icons.g_mobiledata)),
+                IconButton(onPressed: () {}, icon: Icon(Icons.facebook)),
+              ],
+            ),
+          ],
         ),
       ),
     );
