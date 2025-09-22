cde56d27f7227f89ed84dd50321e1ece94566f2a
eedu7 <mueedahmad067@gmail.com>
2025-09-22 15:02:48 +0500

    class AppTheme {   static final light = ThemeData(     brightness: Brightness.light,     colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),     useMaterial3: true,   );
      static final dark = ThemeData(
        brightness: Brightness.dark,
        colorScheme: ColorScheme.fromSeed(
          seedColor: Colors.deepPurple,
          brightness: Brightness.dark,
        ),
        useMaterial3: true,
      );
    }

---
+++ apps/mobile/lib/config/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final light = ThemeData(brightness: Brightness.light);
+
+  static final dark = ThemeData(brightness: Brightness.dark);
+}
